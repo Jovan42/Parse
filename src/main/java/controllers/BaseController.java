74e38c25a02814c55ea2066aada0e79adb8733df
j.manojlovic@vegaitsourcing.rs <j.manojlovic@vegaitsourcing.rs>
2019-12-11 14:54:23 +0100

    Generic controller

--- src/main/java/controllers/BaseController.java
+++ src/main/java/controllers/BaseController.java
@@ -1,5 +1,63 @@
 package controllers;
 
-public interface BaseController {
-  void init();
+import services.BaseService;
+import spark.Spark;
+
+public abstract class BaseController<S extends BaseService> {
+  protected String BASE_URL;
+  protected S service;
+
+  public void init() {
+    get();
+    getById();
+    post();
+    put();
+    delete();
+  }
+
+  public void get() {
+    Spark.get(
+        BASE_URL,
+        (req, res) -> {
+          res.type("application/json");
+          return service.findAll();
+        });
+  }
+
+  public void getById() {
+    Spark.get(
+        BASE_URL + "/:id",
+        (req, res) -> {
+          res.type("application/json");
+          return service.findById(req.params(":id"));
+        });
+  }
+
+  public void post() {
+    Spark.post(
+        BASE_URL,
+        (req, res) -> {
+          res.type("application/json");
+          return service.create(req.body());
+        });
+  }
+
+  public void put() {
+    Spark.put(
+        BASE_URL,
+        (req, res) -> {
+          res.type("application/json");
+          return service.update(req.body());
+        });
+  }
+
+  public void delete(){
+    Spark.delete(
+            BASE_URL + "/:id",
+            (req, res) -> {
+              res.type("application/json");
+              service.delete(req.params(":id"));
+              return "";
+            });
+  }
 }
--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -3,48 +3,12 @@ package controllers;
 import anotations.Controller;
 import services.UserService;
 
-import static spark.Spark.*;
-
 @Controller
-public class UserController implements BaseController {
-
-  private static UserService userService = new UserService();
+public class UserController extends BaseController<UserService> {
 
   public void init() {
-    get(
-        "/users",
-        (req, res) -> {
-          res.type("application/json");
-          return userService.findAll();
-        });
-    get(
-        "/users/:id",
-        (req, res) -> {
-          res.type("application/json");
-          return userService.findById(req.params(":id"));
-        });
-
-    post(
-        "/users",
-        (req, res) -> {
-          res.type("application/json");
-
-          return userService.create(req.body());
-        });
-    put(
-        "/users",
-        (req, res) -> {
-          res.type("application/json");
-
-          return userService.update(req.body());
-        });
-
-    delete(
-        "/users/:id",
-        (req, res) -> {
-          res.type("application/json");
-          userService.delete(req.params(":id"));
-          return "";
-        });
+    service = new UserService();
+    BASE_URL = "/users";
+    super.init();
   }
 }
